08e5d5a55edea9ef85492c681a83c37954cc5525
aishik kirtaniya <kirtuaishik@gmail.com>
2019-05-26 19:48:37 +0530

    splash screen function added

--- app/src/main/java/scholify/app/cybernoob/MainActivity.java
+++ app/src/main/java/scholify/app/cybernoob/MainActivity.java
@@ -1,7 +1,10 @@
 package scholify.app.cybernoob;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(getSupportActionBar() != null)
+        {
+            getSupportActionBar().hide();
+        }
+        SharedPreferences pref = getSharedPreferences("prefs",MODE_PRIVATE);
+        Boolean first = pref.getBoolean("first",true);
+        Boolean Logged = pref.getBoolean("logged",false);
+        if(first)
+        {
+            Toast.makeText(this, "This is your first time,move to tabview", Toast.LENGTH_LONG).show();
+            pref.edit().putBoolean("first",false).apply();
+        }else if(Logged)
+        {
+            Toast.makeText(this, "You are logged in,move to home", Toast.LENGTH_LONG).show();
+        }else
+        {
+            Toast.makeText(this, "You are not Logged in,move to login page", Toast.LENGTH_SHORT).show();
+        }
     }
 }
